0201d00e7147dfe639e1e797dd248b3003c7d4de
KevalKankrecha-2 <kkj63609@gmail.com>
2024-08-11 22:41:39 +0530

    remove: resume

--- src/Components/AboutComponent.js
+++ src/Components/AboutComponent.js
@@ -9,7 +9,7 @@ export default function AboutComponent({ id }) {
         I'm <span> Keval </span>, and I have completed my <span>Bechlore of Computer Engineering </span> from Darshan University, Rajkot. I am
         Proficient in backend technologies, I excel in architecting solutions that translate end-user challenges into intuitive <span>APIs</span>, seamlessly integrating them for enhanced functionality. My expertise extends to <span>database</span> management, ensuring efficient data handling and retrieval, complemented by a keen understanding of frontend technologies for a comprehensive development approach.
         <br /> <br />
-        Beyond my coding expertise, I am deeply spiritual, following the path of <span>Hinduism and Sanatan</span> Dharma, and actively volunteering at <span>BAPS</span> for the past few years. I hold a profound admiration for <span>Mahakal</span>, finding solace and inspiration in his divine essence. Want to know more about me? Feel free to <a className='resume' href="https://drive.google.com/file/d/1hROCoNMpIkGQahB0E7v-OFbXlv9EHJrB/view?usp=sharing" target="_blank" rel="noopener noreferrer">explore my resume.</a>
+        Beyond my coding expertise, I am deeply spiritual, following the path of <span>Hinduism and Sanatan</span> Dharma, and actively volunteering at <span>BAPS</span> for the past few years. I hold a profound admiration for <span>Mahakal</span>, finding solace and inspiration in his divine essence. Want to know more about me? Feel free to <a className='resume' href="#" target="_blank" rel="noopener noreferrer">explore my resume.</a>
       </div>
       <div className="profile-photo">
         <img className="img" src={myProfile} alt="Profile"></img>
